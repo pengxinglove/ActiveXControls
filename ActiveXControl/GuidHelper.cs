653d5d9fc82586af35f36014301d98a4bd13d959
wjw <492677104@qq.com>
2018-09-06 16:30:47 +0800

    create project

---
+++ ActiveXControl/GuidHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ActiveXControl
+{
+    /// <summary>
+    /// Guid帮助类
+    /// </summary>
+    public class GuidHelper
+    {
+        static GuidHelper() { }
+
+        public static string CreateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+    }
+}
---
+++ ActiveXControl/UserControl1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using mshtml;
+
+namespace ActiveXControl
+{
+     [Guid("f469bc9e-bcde-4da7-bd22-465ddfdaf53a")]
+    public partial class UserControl1: UserControl,IObjectSafety
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        #region IObjectSafety 成员
+        private const string _IID_IDispatch = "{00020400-0000-0000-C000-000000000046}";
+        private const string _IID_IDispatchEx = "{a6ef9860-c720-11d0-9337-00a0c90dcaa9}";
+        private const string _IID_IPersistStorage = "{0000010A-0000-0000-C000-000000000046}";
+        private const string _IID_IPersistStream = "{00000109-0000-0000-C000-000000000046}";
+        private const string _IID_IPersistPropertyBag = "{37D84F60-42CB-11CE-8135-00AA004BB851}";
+
+        private const int INTERFACESAFE_FOR_UNTRUSTED_CALLER = 0x00000001;
+        private const int INTERFACESAFE_FOR_UNTRUSTED_DATA = 0x00000002;
+        private const int S_OK = 0;
+        private const int E_FAIL = unchecked((int)0x80004005);
+        private const int E_NOINTERFACE = unchecked((int)0x80004002);
+
+        private bool _fSafeForScripting = true;
+        private bool _fSafeForInitializing = true;
+
+        public int GetInterfaceSafetyOptions(ref Guid riid, ref int pdwSupportedOptions, ref int pdwEnabledOptions)
+        {
+            int Rslt = E_FAIL;
+
+            string strGUID = riid.ToString("B");
+            pdwSupportedOptions = INTERFACESAFE_FOR_UNTRUSTED_CALLER | INTERFACESAFE_FOR_UNTRUSTED_DATA;
+            switch (strGUID)
+            {
+                case _IID_IDispatch:
+                case _IID_IDispatchEx:
+                    Rslt = S_OK;
+                    pdwEnabledOptions = 0;
+                    if (_fSafeForScripting == true)
+                        pdwEnabledOptions = INTERFACESAFE_FOR_UNTRUSTED_CALLER;
+                    break;
+                case _IID_IPersistStorage:
+                case _IID_IPersistStream:
+                case _IID_IPersistPropertyBag:
+                    Rslt = S_OK;
+                    pdwEnabledOptions = 0;
+                    if (_fSafeForInitializing == true)
+                        pdwEnabledOptions = INTERFACESAFE_FOR_UNTRUSTED_DATA;
+                    break;
+                default:
+                    Rslt = E_NOINTERFACE;
+                    break;
+            }
+
+            return Rslt;
+        }
+
+        public int SetInterfaceSafetyOptions(ref Guid riid, int dwOptionSetMask, int dwEnabledOptions)
+        {
+            int Rslt = E_FAIL;
+            string strGUID = riid.ToString("B");
+            switch (strGUID)
+            {
+                case _IID_IDispatch:
+                case _IID_IDispatchEx:
+                    if (((dwEnabledOptions & dwOptionSetMask) == INTERFACESAFE_FOR_UNTRUSTED_CALLER) && (_fSafeForScripting == true))
+                        Rslt = S_OK;
+                    break;
+                case _IID_IPersistStorage:
+                case _IID_IPersistStream:
+                case _IID_IPersistPropertyBag:
+                    if (((dwEnabledOptions & dwOptionSetMask) == INTERFACESAFE_FOR_UNTRUSTED_DATA) && (_fSafeForInitializing == true))
+                        Rslt = S_OK;
+                    break;
+                default:
+                    Rslt = E_NOINTERFACE;
+                    break;
+            }
+
+            return Rslt;
+        }
+        #endregion
+
+        #region 注册回调函数
+        private IHTMLWindow2 temphtml = null;
+        private string functionstr = "";
+
+        public void RegJs(object win, string fuc)
+        {
+            temphtml = (IHTMLWindow2)win;
+            if (temphtml != null && !string.IsNullOrEmpty(fuc))
+            {
+                functionstr = fuc;
+            }
+            else
+            {
+                temphtml = null;
+                functionstr = "";
+                MessageBox.Show("注" +
+                                "册脚本失败！");
+            }
+        }
+        #endregion
+
+        #region 控件初始化方法
+        public void Init(string message)
+        {
+            MessageBox.Show(message);
+        }
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 执行script脚本
+            temphtml.execScript(functionstr + "()", "JScript");
+        }
+    }
+}
